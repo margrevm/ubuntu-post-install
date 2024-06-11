13312e4d54b99271a0de8301e97ebed790a5796b
Mike Margreve <63907112+margrevm@users.noreply.github.com>
2024-06-11 18:46:21 +0200

    Create ubuntu-post-install.sh

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# Post-installation script for Ubuntu 24.04
+
+# ---------------------------------------------------
+# Creating folder structure
+# ---------------------------------------------------
+echo "[Creating the folder structure]"
+
+CREATE_DIRS=(
+	$HOME/projects
+	$HOME/scripts
+	$HOME/src
+)
+
+mkdir -pv ${CREATE_DIRS[@]}
+
+# ---------------------------------------------------
+# APT package installation
+# ---------------------------------------------------
+echo "[Installing apt packages]"
+
+APT_INSTALL_PACKAGES=(
+	tree
+	neofetch
+	vlc
+	htop
+	gnome-tweaks
+	python3
+	python3-distutils
+	nmap
+	wget
+	default-jdk
+	default-jre
+	heif-gdk-pixbuf
+	git
+	curl
+	unzip
+	xclip
+	less
+  ubuntu-restricted-extras
+	code
+)
+
+# Danger zone /!\ Please be careful and make sure to not purge/remove any essential packages
+APT_PURGE_PACKAGES=(
+	geary
+	gnome-contacts
+)
+
+APT_REMOVE_PACKAGES=(
+)
+
+#echo "➜ Adding apt repositories..."
+# Nothing yet
+
+echo "➜ Updating apt repositories..."
+sudo apt update -yq
+
+echo "➜ Installing packages..."
+# Existing packages will not be installed by apt.
+sudo apt install ${APT_INSTALL_PACKAGES[@]} -q
+
+echo "➜ Purging/removing apt packages..."
+# This will remove the package and the configuration files (/etc)
+# Should be used for applications you will never need. If you are not sure use 'apt remove' 
+# instead (uncomment below) which will leave the config files.
+sudo apt purge ${APT_PURGE_PACKAGES[@]} -q
+#sudo apt remove ${APT_REMOVE_PACKAGES[@]}
+
+echo "➜ Removing unused apt package dependencies..."
+# ... packages that are not longer needed
+sudo apt autoremove -q
+
+echo "➜ Upgrading apt packages to their latest version..."
+# 'apt full-upgrade' is an enhanced version of the 'apt upgrade' command. 
+# Apart from upgrading existing software packages, it installs and removes 
+# some packages to satisfy some dependencies. The command includes a smart conflict 
+# resolution feature that ensures that critical packages are upgraded first 
+# at the expense of those considered of a lower priority.
+sudo apt full-upgrade -q
+
+echo "➜ Cleaning package cache..."
+# 'apt autoclean' removes all stored archives in your cache for packages that can not 
+# be downloaded anymore (thus packages that are no longer in the repo or that have a newer version in the repo).
+# You can use 'apt clean' to remove all stored archives in your cache to safe even more disk space.
+sudo apt autoclean -q
+#sudo apt clean
+
+# ---------------------------------------------------
+# Flatpack packages installation
+# ---------------------------------------------------
+# Not supported by default
+
+# ---------------------------------------------------
+# Snap packages installation
+# ---------------------------------------------------
+echo "[Installing snap packages]"
+# Important: Install 'snapd' to support snap packages (available as apt package).
+# Snap is not natively supported by Pop!_OS. The usage of flatpak is recommended.
+SNAP_INSTALL_PACKAGES=(
+	bw
+  spotify
+  joplin-desktop
+  whatsapp-for-linux
+)
+
+echo "➜ Install snap packages..."
+snap install ${SNAP_INSTALL_PACKAGES[@]}
+
+echo "➜ Update snap packages..."
+snap update
+
+# ---------------------------------------------------
+# .deb packages installation (manual)
+# ---------------------------------------------------
+#echo "[Downloading and installing .deb packages manually]"
+
+#echo "➜ Downloading .deb packages..."
+#DL_DIR=$HOME/Downloads/packages
+#mkdir -pv $DL_DIR
+#wget -q --show-progress "<URL>" -P "$DL_DIR"
+
+#echo "➜ Installing .deb packages..."
+#sudo dpkg -i $DL_DIR/*.deb
+#sudo apt install -f
+
+# ---------------------------------------------------
+# Other packages installation (full manual installation)
+# ---------------------------------------------------
+#echo "[Other packages]"
+
+# ---------------------------------------------------
+# Third-party drivers (Nvidia)
+# ---------------------------------------------------
+#echo "[Drivers]"
+sudo ubuntu-drivers autoinstall
+
+# ---------------------------------------------------
+# Custom actions
+# ---------------------------------------------------
+echo "[Custom actions]"
+
+echo "➜ Updating font cache..."
+# Update font cache (required after installing MS fonts)
+sudo fc-cache -f
+
+# ---------------------------------------------------
+# Gnome settings
+# ---------------------------------------------------
+# This really depends on your preferences :-)
+echo "[Applying Gnome settings]"
+
+# Gnome windows:
+# When you click an icon in the launcher, it opens the application. But, you cannot minimize it by clicking it again. This can be changed with the following command.
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+# Enable night light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+# Gedit settings:
+# All gedit related settings can be listed with: gsettings list-recursively | grep -i gedit
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+
+# ---------------------------------------------------
+# Clone git repos
+# ---------------------------------------------------
+echo "[Cloning git repos]"
+
+# Go target directory
+cd $HOME/scripts
+git clone https://github.com/margrevm/ubuntu-post-install
+
+
+# ---------------------------------------------------
+# Summary
+# ---------------------------------------------------
+neofetch
+echo "[Installation completed!]"
+cd $HOME
+
+# Reminders
+# open github website to remind me to set up github
+# firefox https://github.com
