b61b6a64abab4b3aec89c35b7a1670ebba575efb
Mike Margreve <mike.margreve@outlook.com>
2026-01-08 20:58:23 +0100

    Colorized verbosness

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -33,87 +33,91 @@ source "$SETTINGS_FILE"
 # ---------------------------------------------------
 # Creating folder structure
 # ---------------------------------------------------
-echo "[Creating the folder structure]"
+printf '\033[1;32m[Creating the folder structure]\033[0m\n'
 
-mkdir -pv ${CREATE_DIRS[@]}
-rmdir -v ${REMOVE_DIRS[@]}
+mkdir -pv "${CREATE_DIRS[@]}"
+rmdir -v "${REMOVE_DIRS[@]}"
 
 # ---------------------------------------------------
 # APT package installation
 # ---------------------------------------------------
-echo "[Installing apt packages]"
+printf '\033[1;32m[Installing apt packages]\033[0m\n'
 
-echo "➜ Adding apt repositories..."
-sudo add-apt-repository  ppa:zhangsongcui3371/fastfetch 
+printf '\033[0;32m➜ Adding apt repositories...\033[0m\n'
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 
-echo "➜ Updating apt repositories..."
+printf '\033[0;32m➜ Updating apt repositories...\033[0m\n'
 sudo apt update -yq
 
-echo "➜ Installing packages..."
+printf '\033[0;32m➜ Installing packages...\033[0m\n'
 # Existing packages will not be installed by apt.
-sudo apt install ${APT_INSTALL_PACKAGES[@]} -q
+sudo apt install -yq ${APT_INSTALL_PACKAGES[@]}
 
-echo "➜ Purging/removing apt packages..."
+printf '\033[0;32m➜ Purging/removing apt packages...\033[0m\n'
 # This will remove the package and the configuration files (/etc)
 # Should be used for applications you will never need. If you are not sure use 'apt remove' 
 # instead (uncomment below) which will leave the config files.
-sudo apt purge ${APT_PURGE_PACKAGES[@]} -q
+sudo apt purge -yq ${APT_PURGE_PACKAGES[@]}
 #sudo apt remove ${APT_REMOVE_PACKAGES[@]}
 
-echo "➜ Removing unused apt package dependencies..."
+printf '\033[0;32m➜ Removing unused apt package dependencies...\033[0m\n'
 # ... packages that are not longer needed
-sudo apt autoremove -q
+sudo apt autoremove -yq
 
-echo "➜ Upgrading apt packages to their latest version..."
+printf '\033[0;32m➜ Upgrading apt packages to their latest version...\033[0m\n'
 # 'apt full-upgrade' is an enhanced version of the 'apt upgrade' command. 
 # Apart from upgrading existing software packages, it installs and removes 
 # some packages to satisfy some dependencies. The command includes a smart conflict 
 # resolution feature that ensures that critical packages are upgraded first 
 # at the expense of those considered of a lower priority.
-sudo apt full-upgrade -q
+sudo apt full-upgrade -yq
 
-echo "➜ Cleaning package cache..."
+printf '\033[0;32m➜ Cleaning package cache...\033[0m\n'
 # 'apt autoclean' removes all stored archives in your cache for packages that can not 
 # be downloaded anymore (thus packages that are no longer in the repo or that have a newer version in the repo).
 # You can use 'apt clean' to remove all stored archives in your cache to safe even more disk space.
-sudo apt autoclean -q
+sudo apt autoclean -yq
 #sudo apt clean
 
 # ---------------------------------------------------
-# Flatpack packages installation
+# Flatpak packages installation
 # ---------------------------------------------------
-echo "[Installing flatpak packages]"
+printf '\033[1;32m[Installing flatpak packages]\033[0m\n'
 
-echo "➜ Add flatpak repositories..."
+printf '\033[0;32m➜ Add flatpak repositories...\033[0m\n'
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "➜ Install flatpak packages..."
+printf '\033[0;32m➜ Install flatpak packages...\033[0m\n'
 # By default flatpak packages will be installed system wide.
-flatpak install --system ${FLATPAK_INSTALL_PACKAGES[@]}
+flatpak install --system -y ${FLATPAK_INSTALL_PACKAGES[@]}
 
-echo "➜ Udate flatpak packages..."
+printf '\033[0;32m➜ Update flatpak packages...\033[0m\n'
 flatpak update
 
 # ---------------------------------------------------
 # Snap packages installation
 # ---------------------------------------------------
-echo "[Installing snap packages]"
+printf '\033[1;32m[Installing snap packages]\033[0m\n'
 # Important: Install 'snapd' to support snap packages (available as apt package).
 # Snap is not natively supported by Pop!_OS. The usage of flatpak is recommended.
 
-echo "➜ Remove snap packages..."
-snap remove ${SNAP_REMOVE_PACKAGES[@]}
+printf '\033[0;32m➜ Remove snap packages...\033[0m\n'
+snap remove ${SNAP_REMOVE_PACKAGES[@]} || true
 
-echo "➜ Install snap packages..."
+printf '\033[0;32m➜ Install snap packages...\033[0m\n'
 snap install ${SNAP_INSTALL_PACKAGES[@]}
 
-echo "➜ Updating (refreshing) snap packages..."
+printf '\033[0;32m➜ Updating (refreshing) snap packages...\033[0m\n'
 snap refresh
 
 # ---------------------------------------------------
 # .deb packages installation (manual)
 # ---------------------------------------------------
 #echo "[Downloading and installing .deb packages manually]"
+
+#TODO: Add syno client here
+
+
 
 #echo "➜ Downloading .deb packages..."
 #DL_DIR=$HOME/Downloads/packages
@@ -125,7 +129,7 @@ snap refresh
 #sudo apt install -f
 
 # ---------------------------------------------------
-# Other packages installation (full manual installation)
+# Other packages / custom installers
 # ---------------------------------------------------
 echo "[Other packages]"
 
@@ -141,18 +145,17 @@ curl -fsSL https://tailscale.com/install.sh | sh
 # ---------------------------------------------------
 # Third-party drivers (Nvidia, ...)
 # ---------------------------------------------------
-echo "[Drivers]"
+printf '\033[1;32m[Drivers]\033[0m\n'
 
-echo "➜ Updating drivers..."
+printf '\033[0;32m➜ Updating drivers...\033[0m\n'
 sudo ubuntu-drivers install
 
 # ---------------------------------------------------
 # Custom actions
 # ---------------------------------------------------
-echo "[Custom actions]"
+printf '\033[1;32m[Custom actions]\033[0m\n'
 
-echo "➜ Updating font cache..."
-# Update font cache (required after installing MS fonts)
+printf '\033[0;32m➜ Updating font cache...\033[0m\n'
 sudo fc-cache -f
 
 # ---------------------------------------------------
@@ -164,7 +167,6 @@ echo "[Applying Gnome settings]"
 # Gnome windows:
 # When you click an icon in the launcher, it opens the application. But, you cannot minimize it by clicking it again. This can be changed with the following command.
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-# Enable night light
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 # Gedit settings:
 # All gedit related settings can be listed with: gsettings list-recursively | grep -i gedit
@@ -172,13 +174,13 @@ gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
 gsettings set org.gnome.gedit.preferences.editor tabs-size 4
 gsettings set org.gnome.gedit.preferences.editor insert-spaces true
 # Set mutter (gnome window manager) to a higher value to avoid "programm not responding" issues with steam
-# Default is 3000 (3 seconds), setting it to 15000 (15 seconds
-gsettings set org.gnome.mutter check-alive-timeout 15000
+# Default is 3000 (3 seconds), setting it to 30000 (30 seconds)
+gsettings set org.gnome.mutter check-alive-timeout 30000
 
 # ---------------------------------------------------
 # Create SSH key
-#----------------------------------------------------
-echo "[Create SSH key]"
+# ---------------------------------------------------
+printf '\033[1;32m[Create SSH key]\033[0m\n'
 ssh-keygen -t rsa -b 4096 -C "mike.margreve@outlook.com"
 ssh-add ~/.ssh/id_rsa
 
@@ -220,6 +222,6 @@ git --reset hard
 # ---------------------------------------------------
 # Summary
 # ---------------------------------------------------
-neofetch
-echo "[Installation completed!]"
-cd $HOME
+neofetch || true
+printf '\033[1;32m[Installation completed!]\033[0m\n'
+cd "$HOME" || exit 0
